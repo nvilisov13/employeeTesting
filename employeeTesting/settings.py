ea6c6a75f134270a4864e2e20dadd3bdc40e3045
nvilisov13 <nvilisov13@gmail.com>
2024-03-25 04:08:33 +0300

    Remove files TelegramBot.py and config.py

--- employeeTesting/settings.py
+++ employeeTesting/settings.py
@@ -127,6 +127,6 @@ LOGIN_REDIRECT_URL = '/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
